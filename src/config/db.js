76931a0fb380e2704b2a90afcb1b31079e3e4758
124050316-ship-it <124050316@upq.edu.mx>
2026-04-10 21:46:45 -0600

    Cambios

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,23 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST
-});
+const pool = new Pool(
+    process.env.DATABASE_URL 
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+    : {
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST
+    }
+);
 
-pool.on('connect', () => console.log('Conexion exitosa'));
+pool.on('connect', () => console.log('Conexion a la BD exitosa'));
 
 module.exports = pool;
